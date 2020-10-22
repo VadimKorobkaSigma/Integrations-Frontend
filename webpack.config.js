0a2c0c1794a1750b62406348ebfa8ee8a02fae8d
AlexeyK <alexey.kononov@checkmarx.com>
2020-10-22 19:30:56 +0300

    Configured scripts, moved the main page.

--- webpack.config.js
+++ webpack.config.js
@@ -10,18 +10,7 @@ module.exports = {
         path: path.resolve('build'),
         filename: 'bundle.js'
     },
-    module: {
-        rules: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
-                    }
-                }]
-            }
-        ]
+    devServer: {
+        contentBase: path.join(__dirname, './src')
     }
 };
