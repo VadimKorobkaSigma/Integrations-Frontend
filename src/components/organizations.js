d28b149643ee84e0c09e62ee4374cd20343483ee
AlexeyK <alexey.kononov@checkmarx.com>
2020-10-29 00:00:04 +0200

    Introduced Mobx for the Organizations service.

--- src/components/organizations.js
+++ src/components/organizations.js
@@ -2,33 +2,22 @@ import React from "react";
 import scmService from '../services/scmService.ts';
 import {Link} from "react-router-dom";
 import orgService from '../services/organizationService';
+import {observer} from "mobx-react";
 
-export default class Organizations extends React.Component {
+export default observer(class extends React.Component {
     constructor(props) {
         super(props);
-
-        const scm = scmService.getById(this.props.match.params.scmId);
-        this.state = {
-            orgs: [],
-            scm
-        };
-    }
-
-    componentDidMount() {
-        orgService.getOrganizations(this.props.match.params.scmId)
-            .then(orgs => {
-                this.setState({orgs, scm: this.state.scm});
-            });
+        orgService.getOrganizations(this.props.match.params.scmId);
     }
 
     render() {
         return (
             <div>
-                <h2>{this.state.scm.name}</h2>
+                <h2>{scmService.getById(this.props.match.params.scmId).name}</h2>
                 <h3>Organizations</h3>
                 <ul>
                     {
-                        this.state.orgs.map(org =>
+                        orgService.organizations.map(org =>
                             <li key={org.id}>
                                 <Link to={`${this.props.match.url}/${org.id}/repos`}>{org.name}</Link>
                             </li>
@@ -38,4 +27,4 @@ export default class Organizations extends React.Component {
             </div>
         )
     }
-}
+})
--- src/services/organizationService.js
+++ src/services/organizationService.js
@@ -1,15 +1,27 @@
-export default {
-    getOrganizations: function (scmId) {
+import {action, makeAutoObservable} from "mobx";
+
+class OrganizationService {
+    organizations = []
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    getOrganizations(scmId) {
         console.info(`Getting organizations for the '${scmId}' SCM`);
+        const assignOrgs = action(() => {
+            this.organizations = [
+                {id: 'org1', name: 'checkmarx-ltd'},
+                {id: 'org3', name: 'example-com'},
+                {id: 'org42', name: 'test-js'}
+            ]
+        })
+        window.setTimeout(assignOrgs, 1000);
+    }
 
-        return Promise.resolve([
-            {id: 'org1', name: 'checkmarx-ltd'},
-            {id: 'org3', name: 'example-com'},
-            {id: 'org42', name: 'test-js'}
-        ]);
-    },
-
-    getById: function (orgId) {
+    getById(orgId) {
         return Promise.resolve({id: orgId, name: 'checkmarx-ltd'});
     }
 }
+
+export default new OrganizationService();
