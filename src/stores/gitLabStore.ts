7b0d0345bcb06cd5c3f48bbc7de6639d185d9e27
AlexeyK <alexey.kononov@checkmarx.com>
2020-11-18 10:13:16 +0200

    Preparations for implementing the GitLab flow.

--- src/stores/gitLabStore.ts
+++ src/stores/gitLabStore.ts
@@ -1,12 +1,22 @@
 import {ScmStore} from "../dtos/scmStore";
 import ScmConfiguration from "../dtos/scmConfiguration";
+import domWrapper from "../services/domWrapper";
+import authStore from "./authStore";
 
 export default class GitLabStore implements ScmStore {
     id = 'gitlab'
     name = 'GitLab'
 
     generatePageUrl(config: ScmConfiguration): string {
-        console.log(`Redirection flow is not yet implemented for ${this.name}`);
-        return '';
+        const origin = domWrapper.getCurrentOrigin();
+        const query = {
+            client_id: config.clientId,
+            scope: config.scope,
+            redirect_uri: `${origin}/scm/${this.id}/organizations`,
+            response_type: 'code',
+            state: authStore.createAndRememberState()
+        };
+        const queryString = new URLSearchParams(query);
+        return `https://gitlab.com/oauth/authorize?${queryString}`;
     }
 }
