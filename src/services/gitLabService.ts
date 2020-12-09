e4590c9bab9b078b3409ef2be463d712de41f1ad
VadimKorobkaSigma <74961305+VadimKorobkaSigma@users.noreply.github.com>
2020-12-09 18:07:03 +0200

    Fix encoding of scopes

--- src/services/gitLabService.ts
+++ src/services/gitLabService.ts
@@ -11,12 +11,11 @@ export default class GitLabService implements ScmService {
         const origin = domWrapper.getCurrentOrigin();
         const query = {
             client_id: config.clientId,
-            scope: config.scope,
             redirect_uri: `${origin}/scm/${this.id}/organizations`,
             response_type: 'code',
             state: authStore.createAndRememberState(),
         };
         const queryString = new URLSearchParams(query);
-        return `https://gitlab.com/oauth/authorize?${queryString}`;
+        return `https://gitlab.com/oauth/authorize?${queryString}&scope=${config.scope}`;
     }
 }
